6cbb9860fb26ff1425aaaa98d92ee8fa0f4450a5
Samuel <samuel.batistela12@gmail.com>
2022-05-21 11:45:27 -0300

    feat: rota e controller de cadastro

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb+srv://sam:123@cluster0.hp9t5.mongodb.net/livraria-node");
+mongoose.connect(
+  "mongodb+srv://sam:123@cluster0.hp9t5.mongodb.net/livraria-node"
+);
 
 let db = mongoose.connection;
 
